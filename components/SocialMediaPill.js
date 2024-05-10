927789cb579c146830faa12f9528c6725b865ba8
SeansC12 <88256324+SeansC12@users.noreply.github.com>
2024-05-10 12:23:13 +0800

    did until the social media icons

---
+++ components/SocialMediaPill.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+function SocialMediaPillWrapper({ children }) {
+  return (
+    <div className="flex items-center justify-start gap-4">
+      {children}
+    </div>
+  );
+}
+
+function SocialMediaPill({ icon, link }) {
+  return (
+    <Link
+      href={link}
+      className="bg-gray-700 p-[0.325rem] rounded-md"
+    >
+      <Image src={icon} width={25} height={25} />
+    </Link>
+  );
+}
+
+export { SocialMediaPillWrapper, SocialMediaPill };
